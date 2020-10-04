28cab5856d52b94e1f4523ff3ac8e08d7506330a
OganyanRV <47751491+OganyanRV@users.noreply.github.com>
2020-10-05 01:31:12 +0300

    v2 added ssim

--- task1/venv/Include/metrics.py
+++ task1/venv/Include/metrics.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy
 import math
-from Pillow import Image
+from PIL import Image
 
 
 def immse(fi_image:numpy.ndarray, se_image:numpy.ndarray) -> float:
@@ -13,4 +13,16 @@ def psnr(fi_image:numpy.ndarray, se_image:numpy.ndarray) -> float:
         mse=255.0
     return 10 * math.log((255.0/mse),10)
 
-def ssim(fi_image, se_image)-> float:
+def ssim(fi_image:numpy.ndarray, se_image:numpy.ndarray)-> float:
+    intensity_fi = fi_image.mean() # Математическое ожидание (среднее арифметическое)
+    intensity_se = se_image.mean() # Математическое ожидание (среднее арифметическое)
+    contrast_fi = fi_image.var() # Среднеквадратическое отклонение (дисперсия)
+    contrast_fi = sqrt(contrast_fi)
+    contrast_se = se_image.var() # Среднеквадратическое отклонение (дисперсия)
+    contrast_se = sqrt(contrast_se)
+    covariance = numpy.cov(contrast_fi, contrast_se) # Ковариация двух случайных величин
+    const_fi = 0.0003
+    const_se = 0.0005
+    return ((2*intensity_fi*intensity_se + const_fi) * (2*covariance + const_se )) /  ((intensity_fi**2 + intensity_se**2 + const_fi) *( contrast_fi**2 +  contrast_se ** 2 +const_se ))
+
+
